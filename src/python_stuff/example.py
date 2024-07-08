a93a89c3b9f2756687a6f34f7bcc9c1f4ad0963d
Micky Lindsay <mekelnz@gmail.com>
2024-07-08 16:49:49 -0700

    pytest and coverage report output

---
+++ src/python_stuff/example.py
@@ -0,0 +1,10 @@
+"""Contains an example function."""
+
+
+def my_function() -> int:
+    """Always returns 42.
+
+    Returns:
+        Always 42.
+    """
+    return 42
---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+from python_stuff.example import my_function
+
+
+def test_my_function() -> None:
+    assert my_function() == 42
